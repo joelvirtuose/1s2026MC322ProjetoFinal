d2a5bd422b0fb05cd0191a068975ad5e5fb5f853
joelvirtuose <joelvirtuose@yahoo.com.br>
2026-07-07 20:39:41 +0000

    Alteração em Product

--- ecommerce-nexus/src/main/java/com/ecommerce/model/entity/Product.java
+++ ecommerce-nexus/src/main/java/com/ecommerce/model/entity/Product.java
@@ -8,6 +8,13 @@ public class Product {
     private final double price;
     private final int stockQuantity;
 
+    /**
+     * 
+     * @param id
+     * @param name
+     * @param price
+     * @param stockQuantity
+     */
     public Product(String id, String name, double price, int stockQuantity) {
         this.id = id;
         this.name = name;
@@ -24,12 +31,19 @@ public class Product {
     /**
      * PADRÃO WITHER: Modificação sem efeitos colaterais locais.
      * Retorna uma nova instância completa clonando o objeto com o novo dado.
+     * @param newStock
+     * @return
      */
     public Product withStockQuantity(int newStock) {
         return new Product(this.id, this.name, this.price, newStock);
     }
 
-    // Regra de Negócio Rica incorporada na própria entidade (Evita Modelo Anêmico)
+    /**
+     * Regra de Negócio Rica incorporada na própria entidade (Evita Modelo Anêmico)
+     * @param quantity
+     * @return
+     * @throws InsufficientStockException
+     */
     public Product deductStock(int quantity) throws InsufficientStockException {
         if (this.stockQuantity < quantity) {
             throw new InsufficientStockException("Estoque insuficiente para o produto: " + name); // Fail-Fast
