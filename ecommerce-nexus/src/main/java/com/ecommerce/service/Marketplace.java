d5742c1a154d15c96f9881f98ef8e3ddeae11a36
joelvirtuose <joelvirtuose@yahoo.com.br>
2026-07-10 14:58:29 +0000

    Alteração em Marketplace

--- ecommerce-nexus/src/main/java/com/ecommerce/service/Marketplace.java
+++ ecommerce-nexus/src/main/java/com/ecommerce/service/Marketplace.java
@@ -2,15 +2,18 @@ package com.ecommerce.service;
 
 import com.google.gson.Gson;
 import com.ecommerce.model.entity.Product;
+import com.ecommerce.model.entity.Order;
 import com.ecommerce.service.persistence.JsonlRepository;
 import com.ecommerce.service.persistence.Repository;
 
 public class Marketplace {
     private final Repository<Product, String> productRepository;
+    private final Repository<Order, String> orderRepository;
 
     public Marketplace(Gson gson) {
         // Inicializa disparando automaticamente a reidratação cronológica do log histórico
         this.productRepository = new JsonlRepository<>("data/products.jsonl", Product.class, gson);
+        this.orderRepository = new JsonlRepository<>("data/orders.jsonl", Order.class, gson);
     }
 
     /**
@@ -28,5 +31,21 @@ public class Marketplace {
      */
     public void saveProduct(Product product) {
         productRepository.productRepository.save(product);
+    }
+
+    /**
+     * 
+     * @param id
+     */
+    public Order getOrder(String id) {
+        return orderRepository.findById(id);
+    }
+
+    /**
+     * 
+     * @param order
+     */
+    public void saveOrder(Order order) {
+        orderRepository.save(order);
     }
 }
